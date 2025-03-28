634c16dc2e53b651778e2f638c4fc48f110c5cf6
Felipe <felipe.viera@mayor.cl>
2025-03-28 17:02:28 -0300

    Adding form_v1 in app.py

--- app.py
+++ app.py
@@ -3,4 +3,23 @@ import gspread
 from google.oauth2.service_account import Credentials
 import json
 
+# Url = app-form-benyxxbxau4q6xvprzhjpq.streamlit.app
+
 # leer las credenciales desde secrets (Streamlit Cloud)
+service_account_info = st.secrets["gcp_service_account"]
+credentials = Credentials.from_service_account_info(service_account_info)
+client = gspread.authorize(credentials)
+
+# Abrir la hoja de Google Sheets
+SHEET_NAME = "prueba_streamlit"
+sheet = client.open(SHEET_NAME).sheet1
+
+# Estructura Formulario
+st.title("Formulario de Registro de Costos")
+descripcion = st.text_input("Descripción del Gasto")
+monto = st.number_input("Monto del Gasto", min_value=0.0, format="%.2f")
+item = st.selectbox("Ítem", ['Aseo y Ornato', 'Choclo', 'Frambuesas', 'Papas', 'Pasto', 'Peonías', 'Campo General'])
+
+if st.button("Guardar Registro"):
+    sheet.append_row([descripcion, monto, item])
+    st.success("¡Registro guardado con éxito!")
