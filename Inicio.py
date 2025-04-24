8f2f3830fe7f0d2f3a0989f6870420613aecc3bd
Felipe <felipe.viera@mayor.cl>
2025-04-24 15:50:43 -0400

    Fixing buttons on 'Inicio'

--- Inicio.py
+++ Inicio.py
@@ -21,8 +21,8 @@ button_style = """
 st.markdown(button_style, unsafe_allow_html=True)
 
 with col1:
-    if st.button("Ir a Reporte"):
-        st.switch_page("pages/1 Reporte.py")
+    if st.button("Ir a Reporte General"):
+        st.switch_page("pages/1 Reporte General.py")
 
 with col2:
     if st.button("Ir a Formulario de Costos"):
